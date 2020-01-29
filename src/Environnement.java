bbc2de1543efa5d2f69e6adc2407ec3ac8fae223
Antoine <antoine.ganne@hotmail.fr>
2020-01-29 16:31:55 +0100

    env et grille

--- src/Environnement.java
+++ src/Environnement.java
@@ -5,79 +5,42 @@ import java.util.Stack;
 
 
 class Environnement {
-    private final Integer nbAgents;
-    private final ArrayList<Stack<Agent>> colonnes;
-    private final HashMap<Agent, Integer> emplacementDesAgents;
+    private Grille grille;
 
-
-    Environnement(Integer nbAgents) {
-        this.nbAgents = nbAgents;
-        this.colonnes = new ArrayList<>(3);
-        for (int i = 0; i < 3; ++i) {
-            this.colonnes.add(new Stack<>());
-        }
-        this.emplacementDesAgents = new HashMap<>(nbAgents);
+    public Environnement(int m, int n) {
+        this.grille=new Grille(n,m);
     }
 
-    void initialLoadOfAgents(List<Agent> agents) {
-        this.colonnes.get(0).addAll(agents);
-        for (Agent agent : agents) {
-            emplacementDesAgents.put(agent, 0);
-        }
+
+    /**
+     *  place les agents et instancie les objets à trier, chacun sur des cases differentes.
+     * @param agents agents à placer
+     * @param nA nombre d'objet A
+     * @param nB nombre d'objet B
+     */
+    void initializePositionOfElements(List<Agent> agents, int nA, int nB) {
+
     }
 
 
     synchronized void percevoir(Agent agent) {
-        int emplacement = emplacementDesAgents.get(agent);
-        Stack<Agent> colonneCourante = colonnes.get(emplacement);
-        Integer positionDansColonne = colonneCourante.search(agent);
-        Agent blocDessousCourant = null;
-        if (positionDansColonne > 0 && positionDansColonne < colonneCourante.size() && positionDansColonne < nbAgents) {
-            blocDessousCourant = colonneCourante.elementAt(positionDansColonne - 1);
-        }
-        boolean estEnDessousDunBloc = positionDansColonne > 1;
-        Agent blocDessus = null;
-        if (estEnDessousDunBloc) {
-            blocDessus = colonneCourante.get(positionDansColonne - 1);
-        }
-        int place1 = emplacement + 1 == 1 ? 2 : 1;
-        int place2 = emplacement + 1 == 3 ? 2 : 3;
 
-
-        return ;
     }
 
-    synchronized void changerEmplacement(Agent agent, int colonneDestinationId) {
-        int emplacement = emplacementDesAgents.get(agent);
-        Stack<Agent> colonneCourante = colonnes.get(emplacement);
-        Agent agentDessusDePile = colonneCourante.pop();
-        try {
-            if (agentDessusDePile != agent) {
-                throw new Exception("deplacement impossible");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Stack<Agent> colonneDestination = colonnes.get(colonneDestinationId - 1);
-        colonneDestination.add(agent);
 
-        emplacementDesAgents.put(agent, colonneDestinationId - 1);
-
-        printEnvironnement();
-    }
 
     synchronized private void printEnvironnement() {
-        System.out.println("*********************");
-        for (int i = 0; i < colonnes.size(); ++i) {
-            Stack<Agent> colonne = colonnes.get(i);
-            System.out.print("colonne " + (i + 1) + ": {");
-            for (Agent agentAtJ : colonne) {
-                System.out.print(agentAtJ.id + " ");
-            }
-            System.out.print("}\n");
-        }
-        System.out.println();
-        System.out.println();
+//        System.out.println("*********************");
+//        for (int i = 0; i < colonnes.size(); ++i) {
+//            Stack<Agent> colonne = colonnes.get(i);
+//            System.out.print("colonne " + (i + 1) + ": {");
+//            for (Agent agentAtJ : colonne) {
+//                System.out.print(agentAtJ.id + " ");
+//            }
+//            System.out.print("}\n");
+//        }
+//        System.out.println();
+//        System.out.println();
     }
 
 }
--- src/Grille.java
+++ src/Grille.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Grille {
     private int n,m;
-    private Case[] cases;
+
+    public ArrayList<Case> getCases() {
+        ArrayList<Case> cases = new ArrayList<>(m*n);
+        for(int x=0;x<m;++x){
+            cases.addAll(Arrays.asList(this.cases[x]).subList(0, n));
+        }
+        return cases;
+    }
+
+    private Case[][] cases;
+
+    public Grille(int n, int m) {
+        this.n = n;
+        this.m = m;
+        cases=new Case[m][n];
+        for(int x=0;x<m;++x){
+            for(int y=0;y<n;++y){
+                cases[x][y]=new Case(x+1,y+1);
+            }
+        }
+    }
+
+
+
+
 }
