772c26db7cb26a33fd5ad82861fe6b834d45196a
Antoine <antoine.ganne@hotmail.fr>
2020-01-29 16:51:40 +0100

    placement of elements

--- src/Environnement.java
+++ src/Environnement.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 
 class Environnement {
@@ -19,8 +16,28 @@ class Environnement {
      * @param nB nombre d'objet B
      */
     void initializePositionOfElements(List<Agent> agents, int nA, int nB) {
+        ArrayList<Case> casesDisponibles=grille.getCases();
+        Random random=new Random();
+        for(Agent agent:agents){
+            Case rdmCase=casesDisponibles.get(random.nextInt(casesDisponibles.size()));
+            rdmCase.elementSurCase=agent;
+            //TODO: sauvegarder x,y dans agent
+            casesDisponibles.remove(rdmCase);
+        }
 
+        for(int i=0;i<nA;++i){
+            Case rdmCase=casesDisponibles.get(random.nextInt(casesDisponibles.size()));
+            rdmCase.elementSurCase=new ObjetATrier('A');
+            casesDisponibles.remove(rdmCase);
+        }
+        for(int i=0;i<nB;++i){
+            Case rdmCase=casesDisponibles.get(random.nextInt(casesDisponibles.size()));
+            rdmCase.elementSurCase=new ObjetATrier('B');
+            casesDisponibles.remove(rdmCase);
+        }
     }
+
+
 
 
     synchronized void percevoir(Agent agent) {
