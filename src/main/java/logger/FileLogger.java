0d678a4e238a7169b4b3ac3340ffd3b2c33e9dc6
Antoine <antoine.ganne@hotmail.fr>
2020-02-16 18:02:53 +0100

    recuperation logger de mon projet opti discrete

---
+++ src/main/java/logger/FileLogger.java
@@ -0,0 +1,46 @@
+package logger;
+
+import com.sun.istack.internal.NotNull;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+public class FileLogger {
+    static final int LENGTH_TO_SEND=100;
+    static final String CRLF="\r\n";
+    static final String TAB="\t";
+    @NotNull
+    private String filePath;
+    private StringBuilder stringBuilder=new StringBuilder();
+    private Writer fstream;
+
+    public FileLogger(String filePath) {
+        this.filePath = filePath;
+        try{
+            fstream=new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void write(String content){
+        stringBuilder.append(content);
+        try {
+            fstream.write(content);
+            fstream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void writeLine(String content){
+        write(content+CRLF);
+    }
+
+    public void nextLine(){
+        write(CRLF);
+    }
+
+}
